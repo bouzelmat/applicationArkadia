6f0af113908edfd11474fb9dc7e8586ee232f8a2
bouzelmat <162742340+bouzelmat@users.noreply.github.com>
2024-07-07 15:49:39 +0200

    Update header.php

--- views/header.php
+++ views/header.php
@@ -2,10 +2,10 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <link href="/ecf_arkadia/assets/menuNavigation.css" rel="stylesheet">
+    <link href="../assets/menuNavigation.css" rel="stylesheet">
     <link href="../assets/style_page_hero.css" rel="stylesheet"> 
-    <link href="/ecf_arkadia/assets/style_page_accueil.css" rel="stylesheet"> 
-    <link href="/ecf_arkadia/assets/typography.css" rel="stylesheet">
+    <link href="../assets/style_page_accueil.css" rel="stylesheet"> 
+    <link href="../assets/typography.css" rel="stylesheet">
     
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -25,22 +25,23 @@
     <header class="py-4">
         <nav class="navbar navbar-expand-md fixed-top navbar-light bg-success">
             <div class="container">
-                <a class="navbar-brand" href="/ecf_arkadia/views/zoo_arkadia_accueil.php">
-                    <img src="/ecf_arkadia/assets/images/logo/logo_zoo_arkadia.png" alt="Logo du zoo arkadia" class="appBar__logo">
+                <a class="navbar-brand" href="zoo_arkadia_accueil.php">
+                    <img src="../assets/images/logo/logo_zoo_arkadia.png" alt="Logo du zoo arkadia" class="appBar__logo">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                     <ul class="navbar-nav">
-                        <li class="nav-item"><a class="nav-link text-white bg-info nav-hover-effect" href="/ecf_arkadia/views/zoo_arkadia_accueil.php">ACCUEIL</a></li>
-                        <li class="nav-item"><a class="nav-link text-white bg-info nav-hover-effect" href="/ecf_arkadia//views/service.php">SERVICES</a></li>
-                        <li class="nav-item"><a class="nav-link text-white bg-info nav-hover-effect" href="/ecf_arkadia/views/habitats_animaux.php">HABITATS ET ANIMAUX</a></li>
-                        <li class="nav-item"><a class="nav-link text-white bg-info nav-hover-effect" href="/ecf_arkadia//views/login.php">CONNEXION</a></li>
-                        <li class="nav-item"><a class="nav-link text-white bg-info nav-hover-effect" href="/ecf_arkadia/views/contact.php">CONTACTS</a></li>
-                        <li class="nav-item"><a href="/ecf_arkadia/views/billet.php" class="btn btn-primary">ACHETER MON BILLET</a></li>
+                        <li class="nav-item"><a class="nav-link text-white bg-info nav-hover-effect" href="zoo_arkadia_accueil.php">ACCUEIL</a></li>
+                        <li class="nav-item"><a class="nav-link text-white bg-info nav-hover-effect" href="service.php">SERVICES</a></li>
+                        <li class="nav-item"><a class="nav-link text-white bg-info nav-hover-effect" href="habitats_animaux.php">HABITATS ET ANIMAUX</a></li>
+                        <li class="nav-item"><a class="nav-link text-white bg-info nav-hover-effect" href="login.php">CONNEXION</a></li>
+                        <li class="nav-item"><a class="nav-link text-white bg-info nav-hover-effect" href="contact.php">CONTACTS</a></li>
+                        <li class="nav-item"><a href="billet.php" class="btn btn-primary">ACHETER MON BILLET</a></li>
                     </ul>
                 </div>
             </div>
         </nav>
     </header>
+
