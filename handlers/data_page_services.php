3192372704ecc7dc9c039b7fbe96ad7e4c1fc61d
bouzelmat <162742340+bouzelmat@users.noreply.github.com>
2024-07-07 18:34:08 +0200

    Update data_page_services.php

--- handlers/data_page_services.php
+++ handlers/data_page_services.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // ce fichier récupère les données de la bdd, ensuite ces données sont traitées par le fichier services.js pour effectuer l'affichage sur la page service.
 require_once '../config/Database.php';
 require_once '../models/class_service.php';
